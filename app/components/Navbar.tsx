4de6ebe6e366d0aefe9c09dfb447984e62181899
Jordan <jordanroberts333@icloud.com>
2023-04-13 15:56:17 +0100

    Add image manager ui, fixed build issue with eslint

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -11,7 +11,7 @@ const Navbar = () => {
                 <li className={`flex-shrink-0 pb-1 px-1 ${pathname === '/' && 'border-b-2 border-black'}`}><Link href={'/'}>Deployments</Link></li>
                 <li className={`flex-shrink-0 pb-1 px-1 ${pathname === '/ImageManager' && 'border-b-2 border-black'}`}><Link href={'/ImageManager'}>Image Manager</Link></li>
                 <li className={`flex-shrink-0 pb-1 px-1 ${pathname === '/ComponentLibrary' && 'border-b-2 border-black'}`}><Link href={'/ComponentLibrary'}>Components</Link></li>
-                <li className={`flex-shrink-0 pb-1 px-1 ${pathname === '/SvgLibrary' && 'border-b-2 border-black'}`}><Link href={'/SvgLibrary'}>Svg's</Link></li>
+                <li className={`flex-shrink-0 pb-1 px-1 ${pathname === '/SvgLibrary' && 'border-b-2 border-black'}`}><Link href={'/SvgLibrary'}>Svg&apos;s</Link></li>
                 <li className={`flex-shrink-0 pb-1 px-1 ${pathname === '/FontLibrary' && 'border-b-2 border-black'}`}><Link href={'/FontLibrary'}>Fonts</Link></li>
                 <li className={`flex-shrink-0 pb-1 px-1 ${pathname === '/Notes' && 'border-b-2 border-black'}`}><Link href={'/Notes'}>Notes</Link></li>
             </ul>
--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>
+      <body className='min-h-[100svh] flex flex-col'>
         <Header/>
         {children}
       </body>
