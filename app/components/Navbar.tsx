c701950f8e10eb2b22f863e607fb64de4e3bbeb2
Jordan <jordanroberts333@icloud.com>
2023-04-14 15:48:11 +0100

    added active nav item transitions, input files on image manager

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -7,13 +7,31 @@ const Navbar = () => {
     const pathname = usePathname()
     return (
         <nav>
-            <ul className='flex max-w-full overflow-x-auto mt-2 gap-4 ml-1 pr-6'>
-                <li className={`flex-shrink-0 pb-1 px-1 ${pathname === '/' && 'border-b-2 border-black'}`}><Link href={'/'}>Deployments</Link></li>
-                <li className={`flex-shrink-0 pb-1 px-1 ${pathname === '/ImageManager' && 'border-b-2 border-black'}`}><Link href={'/ImageManager'}>Image Manager</Link></li>
-                <li className={`flex-shrink-0 pb-1 px-1 ${pathname === '/ComponentLibrary' && 'border-b-2 border-black'}`}><Link href={'/ComponentLibrary'}>Components</Link></li>
-                <li className={`flex-shrink-0 pb-1 px-1 ${pathname === '/SvgLibrary' && 'border-b-2 border-black'}`}><Link href={'/SvgLibrary'}>Svg&apos;s</Link></li>
-                <li className={`flex-shrink-0 pb-1 px-1 ${pathname === '/FontLibrary' && 'border-b-2 border-black'}`}><Link href={'/FontLibrary'}>Fonts</Link></li>
-                <li className={`flex-shrink-0 pb-1 px-1 ${pathname === '/Notes' && 'border-b-2 border-black'}`}><Link href={'/Notes'}>Notes</Link></li>
+            <ul className='flex max-w-full overflow-x-auto mt-4 gap-4 ml-1 pr-6'>
+                <li className={`flex-shrink-0 pb-1 px-1 relative `}>
+                    <Link href={'/'}>Deployments</Link>
+                    <div className={`absolute bottom-0 border-b-2 translate-x-[-50%] left-[50%] border-black ${pathname === '/' ? 'w-full duration-500' : 'w-0 duration-150'}`}></div>
+                </li>
+                <li className={`flex-shrink-0 pb-1 px-1 relative `}>
+                    <Link href={'/ImageManager'}>Images-Opt</Link>
+                    <div className={`absolute bottom-0 duration-500 border-b-2 translate-x-[-50%] left-[50%] border-black ${pathname === '/ImageManager' ? 'w-full duration-500' : 'w-0 duration-150'}`}></div>
+                </li>
+                <li className={`flex-shrink-0 pb-1 px-1 relative `}>
+                    <Link href={'/SvgLibrary'}>Svg&apos;s</Link>
+                    <div className={`absolute bottom-0 duration-500 border-b-2 translate-x-[-50%] left-[50%] border-black ${pathname === '/SvgLibrary' ? 'w-full duration-500' : 'w-0 duration-150'}`}></div>
+                </li>
+                <li className={`flex-shrink-0 pb-1 px-1 relative `}>
+                    <Link href={'/ComponentLibrary'}>Components</Link>
+                    <div className={`absolute bottom-0 duration-500 border-b-2 translate-x-[-50%] left-[50%] border-black ${pathname === '/ComponentLibrary' ? 'w-full duration-500' : 'w-0 duration-150'}`}></div>
+                </li>
+                <li className={`flex-shrink-0 pb-1 px-1 relative `}>
+                    <Link href={'/FontLibrary'}>Fonts</Link>
+                    <div className={`absolute bottom-0 duration-500 border-b-2 translate-x-[-50%] left-[50%] border-black ${pathname === '/FontLibrary' ? 'w-full duration-500' : 'w-0 duration-150'}`}></div>
+                </li>
+                <li className={`flex-shrink-0 pb-1 px-1 relative `}>
+                    <Link href={'/Notes'}>Notes</Link>
+                    <div className={`absolute bottom-0 duration-500 border-b-2 translate-x-[-50%] left-[50%] border-black ${pathname === '/Notes' ? 'w-full duration-500' : 'w-0 duration-150'}`}></div>
+                </li>
             </ul>
         </nav>
     )
